76e44a1fe87012e05605f32276176bec938ecedf
bugorcka <37016499+bugorcka@users.noreply.github.com>
2024-03-07 16:33:43 +0300

    Update prom_test.go

--- src/karlsenstratum/prom_test.go
+++ src/karlsenstratum/prom_test.go
@@ -3,8 +3,8 @@ package karlsenstratum
 import (
 	"testing"
 
-	"github.com/karlsen-network/karlsen-stratum-bridge/src/gostratum"
-	"github.com/karlsen-network/karlsend/app/appmessage"
+	"github.com/bugorcka/nautilusstratum/src/gostratum"
+	"github.com/Nautilus-Network/nautiliad/app/appmessage"
 )
 
 func TestPromValid(t *testing.T) {
